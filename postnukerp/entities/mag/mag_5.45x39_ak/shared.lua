e0dbca941500b6f27100f5cbc33b1ecdfe332128
AndyClausen <andreas.clausen@hotmail.com>
2016-03-14 18:42:43 +0100

    Added files via upload

---
+++ postnukerp/entities/mag/mag_5.45x39_ak/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+ENT.PrintName		= "Empty AK 5.45 MAG"
+ENT.Author			= "H.A.Z.G"
+ENT.Purpose			= "4shootinkikes"
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
