9ab0b8ccf2cdb14a62da99c755ca426c1753ac1a
AndyClausen <andreas.clausen@hotmail.com>
2016-03-14 19:29:43 +0100

    Added files via upload

---
+++ postnukerp/entities/entities/part_ar-15_grip/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+ENT.PrintName		= "part_ar-15_grip"
+ENT.Author			= "H.A.Z.G"
+ENT.Purpose			= "Intermediary Item"
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
---
+++ postnukerp/entities/entities/part_ks-23_receiver/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+ENT.PrintName		= "part_ks-23_receiver"
+ENT.Author			= "H.A.Z.G"
+ENT.Purpose			= "Intermediary Item"
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
