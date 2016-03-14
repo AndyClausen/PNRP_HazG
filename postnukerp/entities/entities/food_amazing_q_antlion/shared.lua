a59e312d7e790bef41e1ca953d52242047d1a209
AndyClausen <andreas.clausen@hotmail.com>
2016-03-14 18:44:46 +0100

    Added files via upload

---
+++ postnukerp/entities/entities/food_amazing_q_antlion/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+ENT.PrintName		= "food_amazing_quality_antlion"
+ENT.Author			= "H.A.Z.G"
+ENT.Purpose			= "LV5 cheese"
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
